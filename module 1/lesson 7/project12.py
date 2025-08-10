d71dc9d49241f99879a4a7e9abab41d6b596332b
jimehgurt <zain.100.12l@gmail.com>
2025-08-10 11:42:04 +0500

    activity 1 and 2

---
+++ module 1/lesson 7/project12.py
@@ -0,0 +1,19 @@
+a = 50
+b = 100
+c = 10
+if a and b and c :
+    print("atleast one is booleans")
+else :
+    print ("these are not booleans")
+d = +10
+e = -10
+if d > 0 or e > 0 :
+    print ("either of them are greater than 0")
+else :
+    print ("smaller than 0")
+f = 5 
+n = 6
+if f != n :
+    print ("this is true")
+else :
+    print ("this is faulse")
