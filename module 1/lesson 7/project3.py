87b509fe9510d0ef8d49fc52dd7a8a771df50a29
jimehgurt <zain.100.12l@gmail.com>
2025-08-16 04:33:45 +0500

    project 3 leson 7

---
+++ module 1/lesson 7/project3.py
@@ -0,0 +1,8 @@
+a = (input("whats your number/letter"))
+if a.isdigit():
+    print("You entered a number.")
+elif a.isalpha():
+    print("You entered an alphabet.")
+else : 
+    print ("your answer is invalid")
+
