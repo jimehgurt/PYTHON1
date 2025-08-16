5884a17e463de0f576932e92cf719ca17b0c29a1
jimehgurt <zain.100.12l@gmail.com>
2025-08-16 11:30:24 +0500

    project1.py

---
+++ module 1/lesson 8/activity1.py
@@ -0,0 +1,14 @@
+x = 5
+if  type(x) is int :
+    print ("this is an integer")
+else :
+    print ("this is not an integer")
+b = 5.2343
+if type(b) is float :
+    print ("this is a float")
+else : ("this is not a float")
+c = "iwejio"
+if type(c) is str :
+    print ("this is a string")
+else : 
+    print ("this is not a string")
