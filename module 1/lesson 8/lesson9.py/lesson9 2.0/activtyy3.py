23ad9cb0d6a5623ded7c9eb82d367a91985c723b
jimehgurt <zain.100.12l@gmail.com>
2025-08-18 12:34:29 +0500

    activity3

---
+++ module 1/lesson 8/lesson9.py/lesson9 2.0/activtyy3.py
@@ -0,0 +1,10 @@
+w_mean = 45
+w_number = 68
+c_number = 56
+total_num = 40
+sum = w_mean * total_num
+print ("this is the wrong sum", sum)
+
+c_sum = sum - (w_number - c_number)
+c_mean = c_sum / total_num
+print ("the correct mean is", c_mean)
